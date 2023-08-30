38d7064ffca9a889a233fe2a39520af6ee36fad1
Rita de Cassia <115326917+RitadeCassiabl@users.noreply.github.com>
2023-08-29 22:55:42 -0300

    criação da rota soma

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,18 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get('/rota_inicial', (req, resp) => 
-resp.status(200).send("Bem vindo a primeira rota!")
-);
+app.get('/rota_inicial', (req, resp) => {
+    resp.status(200).send("Bem vindo a primeira rota!")
+});
+
+app.get('/somar', (req, resp) => {
+    const y = +req.query.y; 
+    const x = +req.query.x; 
+
+    resp.json({ soma: `${y+x}`});
+});
 
 app.listen(3000, () => 
 console.log('Server funcionando :) na porta 3000')
