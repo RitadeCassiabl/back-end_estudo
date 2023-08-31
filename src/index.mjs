cead45ad13045971f61855a9df14d54d13b38481
Rita de Cassia <115326917+RitadeCassiabl@users.noreply.github.com>
2023-08-31 17:06:18 -0300

    troca da ordem de x e y

--- src/index.mjs
+++ src/index.mjs
@@ -19,7 +19,7 @@ app.get("/somar", (req, resp) => {
           "Os valores de y e x devem ser números! faça a requisição novamente",
       });
   }
-  resp.status(200).json({ resultado: y + x });
+  resp.status(200).json({ resultado: x + y });
 });
 
 app.listen(3000, () => console.log("Server funcionando :) na porta 3000"));
