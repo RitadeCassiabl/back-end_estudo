4450c6b61272374ce516e44a416b45df0cbc9373
guilherme-gjv <guijesus.info@gmail.com>
2023-08-31 10:46:10 -0300

    mudando os atributos para snake_case

--- src/index.mjs
+++ src/index.mjs
@@ -17,7 +17,7 @@ app.get("/somar", (req, resp) => {
         "Os valores de y e x devem ser números! faça a requisição novamente",
     });
   }
-  resp.status(200).json({ Resultado: y + x });
+  resp.status(200).json({ resultado: y + x });
 });
 
 app.get("/subtrair", (req, resp) => {
@@ -43,7 +43,7 @@ app.get("/multiplicar", (req, resp) => {
         "Os valores de y e x devem ser números! faça a requisição novamente",
     });
   }
-  resp.status(200).json({ Resultado: y * x });
+  resp.status(200).json({ resultado: y * x });
 });
 
 app.listen(3000, () => console.log("Server funcionando :) na porta 3000"));
