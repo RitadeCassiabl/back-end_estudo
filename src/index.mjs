6edec2ca6fee9ee700bd8fcbaf48a065a9c1db62
Rita de Cassia <115326917+RitadeCassiabl@users.noreply.github.com>
2023-08-31 16:54:53 -0300

    rota dividir: validação e troca da ordem de x e y

--- src/index.mjs
+++ src/index.mjs
@@ -50,15 +50,23 @@ app.get("/dividir", (req, resp) => {
   const y = +req.query.y;
   const x = +req.query.x;
 
-  if (Number.isNaN(y) || Number.isNaN(x)) {
+  if (Number.isNaN(x) || Number.isNaN(y)) {
     resp
       .status(400)
       .json({
         error:
-          "Os valores de y e x devem ser números! faça a requisição novamente",
+          "Os valores de y e x devem ser números ! faça a requisição novamente",
       });
   }
-  resp.status(200).json({ Resultado: y / x });
+  if(x == 0 || y == 0){
+    resp
+      .status(400)
+      .json({
+        error:
+          "Não há como dividir um número por 0 ! faça a requisição novamente"
+      })
+  }
+  resp.status(200).json({ resultado: x / y });
 });
 
 app.listen(3000, () => console.log("Server funcionando :) na porta 3000"));
