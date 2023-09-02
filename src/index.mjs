04d0c092e19a9b26fcb112ec13da35c52fa934c2
Rita de Cassia <115326917+RitadeCassiabl@users.noreply.github.com>
2023-09-01 21:46:16 -0300

    alterações rota /dividir

--- src/index.mjs
+++ src/index.mjs
@@ -57,16 +57,16 @@ app.get("/dividir", (req, resp) => {
         error:
           "Os valores de y e x devem ser números ! faça a requisição novamente",
       });
-  }
-  if(x == 0 || y == 0){
+  }else if(y == 0){
     resp
       .status(400)
       .json({
         error:
           "Não há como dividir um número por 0 ! faça a requisição novamente"
       })
-  }
-  resp.status(200).json({ resultado: x / y });
+  }else{
+    resp.status(200).json({ resultado: x / y });
+  } 
 });
 
 app.listen(3000, () => console.log("Server funcionando :) na porta 3000"));
