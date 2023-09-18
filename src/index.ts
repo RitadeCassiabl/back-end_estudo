594f5b466c88a3a77dccbb23070fa0c80e2df7e3
Rita de Cassia <115326917+RitadeCassiabl@users.noreply.github.com>
2023-09-18 16:32:50 -0300

    finalizando a transição para ts

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,69 @@
 import express from "express";
+import { Request } from "express";
+import { Response } from "express";
 
 const app = express();
 app.use(express.json());
 
-app.get("/rota_inicial", (req, res) => {
+app.get("/rota_inicial", (req:Request, res:Response) => {
   res.status(200).send("Bem vindo a primeira rota!");
 });
 
-app.get("/somar", (req, res) => {
+app.get("/somar", (req:Request, res:Response) => {
   let y: number;
   let x: number;
 
   if (req.query.x && req.query.y) {
     x = +req.query.x;
     y = +req.query.y;
-    res.status(200).json({ resultado: x + y });
-  } else {
-    res.status(400).json({
-      error:
-        "Os valores de y e x devem ser números! faça a requisição novamente",
-    });
+    if (Number.isNaN(x) || Number.isNaN(y)) {
+      res.status(400).json({
+        error:
+          "Os valores de y e x devem ser números! faça a requisição novamente",
+      });
+    } else {
+      res.status(200).json({ resultado: x + y });
+    }
   }
 });
 
-app.get("/subtrair", (req, res) => {
+app.get("/subtrair", (req:Request, res:Response) => {
   let y: number;
   let x: number;
 
   if (req.query.x && req.query.y) {
     x = +req.query.x;
     y = +req.query.y;
-    res.status(200).json({ resultado: x - y });
-  } else {
-    res.status(400).json({
-      error:
-        "Os valores de y e x devem ser números! faça a requisição novamente",
-    });
+    if (Number.isNaN(x) || Number.isNaN(y)) {
+      res.status(400).json({
+        error:
+          "Os valores de y e x devem ser números! faça a requisição novamente",
+      });
+    } else {
+      res.status(200).json({ resultado: x - y });
+    }
   }
 });
 
-app.get("/multiplicar", (req, res) => {
+app.get("/multiplicar", (req:Request, res:Response) => {
   let y: number;
   let x: number;
 
   if (req.query.x && req.query.y) {
     x = +req.query.x;
     y = +req.query.y;
-    res.status(200).json({ resultado: x * y });
-  } else {
-    res.status(400).json({
-      error:
-        "Os valores de y e x devem ser números! faça a requisição novamente",
-    });
+    if (Number.isNaN(x) || Number.isNaN(y)) {
+      res.status(400).json({
+        error:
+          "Os valores de y e x devem ser números! faça a requisição novamente",
+      });
+    } else {
+      res.status(200).json({ resultado: x * y });
+    }
   }
 });
 
-app.get("/dividir", (req, res) => {
+app.get("/dividir", (req:Request, res:Response) => {
   let y: number;
   let x: number;
 
@@ -66,14 +74,15 @@ app.get("/dividir", (req, res) => {
       res.status(400).json({
         error: "Não há como dividir um número por zero!",
       });
-    } else {
-      res.status(200).json({ resultado: x / y });
+      if (Number.isNaN(x) || Number.isNaN(y)) {
+        res.status(400).json({
+          error:
+            "Os valores de y e x devem ser números! faça a requisição novamente",
+        });
+      } else {
+        res.status(200).json({ resultado: x / y });
+      }
     }
-  } else {
-    res.status(400).json({
-      error:
-        "Os valores de y e x devem ser números! faça a requisição novamente",
-    });
   }
 });
 
